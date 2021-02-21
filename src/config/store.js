e69a099f02b1a53ac3b90c7f96628538e4b84efe
unknown <piyush96arora@gmail.com>
2021-02-21 15:09:09 +0530

    test

--- src/config/store.js
+++ src/config/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducers from './rootReducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import rootReducers from './rootReducer';
 
 const middleware = [thunk];
 const initialState = {};
